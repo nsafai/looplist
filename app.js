803a00785a44b55d187bcb8d69df8b203e568647
Nicolai Safai <nicolaisafai@gmail.com>
2019-05-02 17:55:35 -0700

    cleanup of things that aren't necessary if using docker

--- app.js
+++ app.js
@@ -2,10 +2,6 @@
 require('dotenv').config()
 require('bcryptjs')
 
-const { MLAB_URI } = process.env
-const { DB_USER } = process.env
-const { DB_PASSWORD } = process.env
-
 const createError = require('http-errors')
 const express = require('express')
 const path = require('path')
@@ -18,8 +14,6 @@ const server = require('http').Server(app)
 // Database setup
 const mongoose = require('mongoose')
 
-// const mlabURI = `mongodb://${DB_USER}:${DB_PASSWORD}@${MLAB_URI}`
-// mongoose.connect(process.env.MONGODB_URI || mlabURI, { useNewUrlParser: true })
 mongoose.connect('mongodb://mongo:27017');
 mongoose.set('useCreateIndex', true) // silencing a deprecated feature warning that's a bug per https://github.com/Automattic/mongoose/issues/6890
 mongoose.Promise = global.Promise
